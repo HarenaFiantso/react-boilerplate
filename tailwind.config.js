5e3b73d28c7f5b1e4b1eb103587bd7ed08fd9ad8
Fiantso Harena <fiantsorav@gmail.com>
2024-01-29 20:59:50 +0300

    style: add my color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        skinColor: '#011842',
+        firstColor: '#6E57E0',
+        firstColorSecond: '#382A83',
+        firstColorAlt: '#5A43CB',
+        firstColorLighter: '#C1B6FC',
+        titleColor: '#242329',
+        textColor: '#6D6A7C',
+        textColorLight: '#A19FAD',
+        inputColor: '#F0EEFC',
+        bodyColor: '#FBFBFE',
+        containerColor: '#EBEBEB',
+      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
