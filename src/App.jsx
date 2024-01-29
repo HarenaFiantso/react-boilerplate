5faaba0621e445a6bf12fefb8994de854b85a7ae
Fiantso Harena <fiantsorav@gmail.com>
2024-01-29 18:46:26 +0300

    chore: add poppins as default font family

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1 className='h-[100vh] bg-black text-center font-bold text-white'>Hello, this is fulldream</h1>;
+  return <h1 className='font-poppins h-[100vh] bg-black text-center font-bold text-white'>Hello, this is fulldream</h1>;
 }
