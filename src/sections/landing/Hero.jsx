0d0a421eb2f041ca46f388262e0d1c50c1343e2f
Fiantso Harena <fiantsorav@gmail.com>
2024-01-30 18:29:39 +0300

    chore: add textVariant motion design

--- src/sections/landing/Hero.jsx
+++ src/sections/landing/Hero.jsx
@@ -1,10 +1,19 @@
+import { staggerContainer } from '../../utils/motion';
 import { motion } from 'framer-motion';
 
 export default function Hero() {
   return (
     <section className='xs:py-8 py-12 pl-6 sm:py-16 sm:pl-16'>
-      <motion.div>
-        <div></div>
+      <motion.div
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}
+        className='mx-auto flex w-full flex-col 2xl:max-w-[1280px]'
+      >
+        <div className='relative z-10 flex flex-col items-center justify-center'>
+          <motion.h1 variants={}>Boilerplate</motion.h1>
+        </div>
       </motion.div>
     </section>
   );
--- src/utils/motion.js
+++ src/utils/motion.js
@@ -28,3 +28,19 @@ export const staggerContainer = (staggerChildren, delayChildren) => ({
     },
   },
 });
+
+export const textVariant = (delay) => ({
+  hidden: {
+    y: 50,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      duration: 1.25,
+      delay,
+    },
+  },
+});
