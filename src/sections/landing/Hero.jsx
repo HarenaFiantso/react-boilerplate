6d164391e174d635a419d2e0976210e4e4b3a29c
Fiantso Harena <fiantsorav@gmail.com>
2024-01-30 18:46:05 +0300

    chore: add slideIn motion design

--- src/sections/landing/Hero.jsx
+++ src/sections/landing/Hero.jsx
@@ -1,4 +1,4 @@
-import { staggerContainer } from '../../utils/motion';
+import { staggerContainer, textVariant } from '../../utils/motion';
 import { motion } from 'framer-motion';
 
 export default function Hero() {
@@ -12,8 +12,30 @@ export default function Hero() {
         className='mx-auto flex w-full flex-col 2xl:max-w-[1280px]'
       >
         <div className='relative z-10 flex flex-col items-center justify-center'>
-          <motion.h1 variants={}>Boilerplate</motion.h1>
+          <motion.h1
+            variants={textVariant(1.1)}
+            className='text-[44px] font-bold uppercase leading-[64.4px] text-white sm:text-[60px] sm:leading-[74.4px] md:text-[100px] md:leading-[114.4px] lg:text-[144px] lg:leading-[158.4px]'
+          >
+            Boilerplate
+          </motion.h1>
+          <motion.div
+            variants={textVariant(1.2)}
+            className='flex flex-row items-center justify-center'
+          >
+            <h1 className='text-[40px] font-bold uppercase leading-[64.4px] text-white sm:text-[60px] sm:leading-[54.4px] md:text-[80px] md:leading-[104.4px] lg:text-[134px] lg:leading-[138.4px]'>
+              full
+            </h1>
+            <div className='mx-[6px] h-[38px] w-[60px] rounded-r-[50px] border-[9px] border-white sm:mx-2 sm:h-[48px] sm:w-[80px] md:h-[108px] md:w-[212px] md:border-[18px]' />
+            <h1 className='text-[40px] font-bold uppercase leading-[64.4px] text-white sm:text-[60px] sm:leading-[54.4px] md:text-[80px] md:leading-[104.4px] lg:text-[134px] lg:leading-[138.4px]'>
+              ream
+            </h1>
+            <div></div>
+          </motion.div>
         </div>
+
+        <motion.div variants={}>
+          <div className='hero-gradient absolute -top-[30px] z-[0] h-[300px] w-full rounded-tl-[140px]'></div>
+        </motion.div>
       </motion.div>
     </section>
   );
