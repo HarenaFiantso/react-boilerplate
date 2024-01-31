0dcf94af692bc7aebbe612e24e285747b1e68514
Fiantso Harena <fiantsorav@gmail.com>
2024-01-31 09:40:33 +0300

    style: add gradient 2 styles

--- src/sections/landing/About.jsx
+++ src/sections/landing/About.jsx
@@ -1,3 +1,7 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <section className='xs:p-8 relative z-10 px-6 py-12 sm:p-16'>
+      <div className=''></div>
+    </section>
+  );
 }
