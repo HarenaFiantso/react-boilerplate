1aba983f50c15c550df9b6d19e60d7bbf63ac444
Fiantso Harena <fiantsorav@gmail.com>
2024-01-31 09:57:16 +0300

    chore: add textVariant2 motion design

--- src/components/landing/CustomTexts.jsx
+++ src/components/landing/CustomTexts.jsx
@@ -2,5 +2,12 @@ import { textContainer } from '../../utils/motion';
 import { motion } from 'framer-motion';
 
 export const TypingText = ({ title, textStyles }) => {
-  <motion.p variants={textContainer}></motion.p>;
+  <motion.p
+    variants={textContainer}
+    className={`text-[14px] font-normal text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((letter, index) => (
+        <motion.span></motion.span>
+    ))}
+  </motion.p>;
 };
--- src/utils/motion.js
+++ src/utils/motion.js
@@ -71,3 +71,18 @@ export const textContainer = {
     transition: { staggerChildren: 0.1, delayChildren: i * 0.1 },
   }),
 };
+
+export const textVariant2 = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'tween',
+      ease: 'easeIn',
+    },
+  },
+};
