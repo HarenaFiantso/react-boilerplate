586f6f8a09a7fc89be3dda2e7b52dc02130caf58
Fiantso Harena <fiantsorav@gmail.com>
2024-02-02 07:50:21 +0300

    chore: add new variants of motion

--- src/sections/landing/GetStarted.jsx
+++ src/sections/landing/GetStarted.jsx
@@ -1,3 +1,18 @@
+import { staggerContainer } from '../../utils/motion';
+import { motion } from 'framer-motion';
+
 export default function GetStarted() {
-  return <h1>Get Started</h1>;
+  return (
+    <section className='xs:p-8 relative z-10 px-6 py-12 sm:p-16'>
+      <motion.div
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}
+        className='mx-auto flex w-full flex-col gap-8 lg:flex-row 2xl:max-w-[1280px]'
+      >
+        <motion.div></motion.div>
+      </motion.div>
+    </section>
+  );
 }
