d9b8518ca2f4654d4e6d36c46c263a66f54d70c6
Fiantso Harena <fiantsorav@gmail.com>
2024-02-02 07:21:10 +0300

    chore: create a new explore world constants

--- src/sections/landing/Explore.jsx
+++ src/sections/landing/Explore.jsx
@@ -1,6 +1,39 @@
+import Planet_1 from '../../assets/landing/planet-01.png';
+import Planet_2 from '../../assets/landing/planet-02.png';
+import Planet_3 from '../../assets/landing/planet-03.png';
+import Planet_4 from '../../assets/landing/planet-04.png';
+import Planet_5 from '../../assets/landing/planet-05.png';
 import { TitleText, TypingText } from '../../components/landing/CustomTexts';
 import { staggerContainer } from '../../utils/motion';
 import { motion } from 'framer-motion';
+
+const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: Planet_1,
+    title: 'Lorem',
+  },
+  {
+    id: 'world-2',
+    imgUrl: Planet_2,
+    title: 'Lorem',
+  },
+  {
+    id: 'world-3',
+    imgUrl: Planet_3,
+    title: 'Lorem',
+  },
+  {
+    id: 'world-4',
+    imgUrl: Planet_4,
+    title: 'Lorem',
+  },
+  {
+    id: 'world-5',
+    imgUrl: Planet_5,
+    title: 'Lorem',
+  },
+];
 
 export default function Explore() {
   return (
@@ -22,6 +55,11 @@ export default function Explore() {
           }
           textStyles='text-center'
         />
+        <div className='mt-[50px] flex min-h-[70vh] flex-col gap-5 lg:flex-row'>
+          {exploreWorlds.map((world, index) => (
+            <h1>Explore the world</h1>
+          ))}
+        </div>
       </motion.div>
     </section>
   );
