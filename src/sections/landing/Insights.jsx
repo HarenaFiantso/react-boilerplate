00ea69f497ef35e4ad0b4669b5303e43e9b7b848
Fiantso Harena <fiantsorav@gmail.com>
2024-02-03 09:35:51 +0300

    chore: create a new insight constants

--- src/sections/landing/Insights.jsx
+++ src/sections/landing/Insights.jsx
@@ -1,3 +1,52 @@
+import Planet1 from '../../assets/landing/planet-06.png';
+import Planet2 from '../../assets/landing/planet-07.png';
+import Planet3 from '../../assets/landing/planet-08.png';
+import { TitleText, TypingText } from '../../components/landing/CustomTexts';
+import { staggerContainer } from '../../utils/motion';
+import { motion } from 'framer-motion';
+
+export const insights = [
+  {
+    imgUrl: Planet1,
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    subtitle:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio facere dignissimos quis, neque velit odio impedit praesentium aperiam porro soluta ipsa dolores modi officia quod architecto corporis? Ratione, perspiciatis dignissimos?',
+  },
+  {
+    imgUrl: Planet2,
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    subtitle:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio facere dignissimos quis, neque velit odio impedit praesentium aperiam porro soluta ipsa dolores modi officia quod architecto corporis? Ratione, perspiciatis dignissimos?',
+  },
+  {
+    imgUrl: Planet3,
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    subtitle:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio facere dignissimos quis, neque velit odio impedit praesentium aperiam porro soluta ipsa dolores modi officia quod architecto corporis? Ratione, perspiciatis dignissimos?',
+  },
+];
+
 export default function Insights() {
-  return <h1>Insights</h1>;
+  return (
+    <section className='xs:p-8 relative z-10 px-6 py-12 sm:p-16'>
+      <motion.div
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}
+        className='mx-auto flex w-full flex-col 2xl:max-w-[1280px]'
+      >
+        <TypingText title='| Insight' textStyles='text-center' />
+        <TitleText
+          title={<>Insight about this boilerplate</>}
+          textStyles='text-center'
+        />
+        <div className='mt-[50px] flex flex-col gap-[30px]'>
+          {insights.map((item, index) => {
+            <h1>Something good here</h1>
+          })}
+        </div>
+      </motion.div>
+    </section>
+  );
 }
